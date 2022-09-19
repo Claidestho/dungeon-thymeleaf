489a2e2012e07e85ccba3ba999e63cab8f2083bc
Loris GULISANO <loris.gulisano@Digital-Grenoble.local>
2022-09-19 13:20:17 +0200

    First commit

---
+++ src/main/java/com/application/dungeonsthymeleaf/DungeonsThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.application.dungeonsthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/dungeonsthymeleaf/DungeonsThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.dungeonsthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
