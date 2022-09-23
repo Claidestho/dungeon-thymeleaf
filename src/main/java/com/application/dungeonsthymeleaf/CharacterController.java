ec245122a82b6982180caa7a12443af7296de86e
Loris GULISANO <loris.gulisano@Digital-Grenoble.local>
2022-09-23 09:26:28 +0200

    Database connection

--- src/main/java/com/application/dungeonsthymeleaf/CharacterController.java
+++ src/main/java/com/application/dungeonsthymeleaf/CharacterController.java
@@ -1,35 +1,22 @@
 package com.application.dungeonsthymeleaf;
 
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpHeaders;
-
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
 @Controller
 public class CharacterController {
-    public List<Character> charactersList;
     public RestTemplate restTemplate = new RestTemplate();
-
-    public CharacterController() {
-        this.charactersList = new ArrayList<>();
-        charactersList.add(0, new Character(1, "Loris", "Magicien", 12));
-        charactersList.add(1, new Character(2, "Allan", "Prêtre", 8));
-        charactersList.add(2, new Character(3, "Jipékof", "Démoniste", 20));
-    }
 
     // Affichage de la liste des personnages
     @RequestMapping("/characters")
@@ -59,7 +46,7 @@ public class CharacterController {
 
     // Récupération de la requête POST du formulaire
     @PostMapping("/characters/add")
-    public String submitCharacterForm(@ModelAttribute("newCharacter") Character newCharacter, HttpServletRequest request) throws URISyntaxException {
+    public String submitCharacterForm(@ModelAttribute("newCharacter") Character newCharacter) throws URISyntaxException {
         Character newNewCharacter = new Character();
         newNewCharacter.setName(newCharacter.getName());
         newNewCharacter.setType(newCharacter.getType());
@@ -71,18 +58,22 @@ public class CharacterController {
         URI uri = new URI("http://localhost:8081/characters");
         HttpEntity<Character> httpEntity = new HttpEntity<>(newNewCharacter, headers);
 
-        newNewCharacter = restTemplate.postForObject(uri, httpEntity, Character.class);
+        restTemplate.postForObject(uri, httpEntity, Character.class);
         return "redirect:/characters/";
     }
 
     @GetMapping("/characters/edit/{id}")
     public String showEditCharacter(Model model, @PathVariable("id") int id) {
-        model.addAttribute("character", charactersList.stream().filter(elt -> elt.getId() == id).findFirst().orElse(null));
+        Character toEdit = restTemplate.getForObject("http://localhost:8081/characters/" + id, Character.class);
+        model.addAttribute("character", toEdit);
         return "editCharacter";
     }
 
+    @GetMapping("/error")
+
+
     @PutMapping("/characters/edit/{id}")
-    public String submitEditForm(@ModelAttribute("character") Character editedCharacter, @PathVariable("id") int id) throws URISyntaxException {
+    public String submitEditForm(@ModelAttribute("character") Character editedCharacter, @PathVariable("id") int id) throws Exception {
         editedCharacter.setId(id);
        restTemplate.put("http://localhost:8081/characters/" + id, editedCharacter, Character.class);
         return "redirect:/characters";
